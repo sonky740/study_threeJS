604ca3c4d6d4e004b047b4bf2e34b5df55c26cd2
Sonky <sonky740@gmail.com>
2023-01-16 00:09:48 +0900

    MeshToonMaterial, MeshNormalMaterial, MeshMatcapMaterial, EnvironmentMap, Skybox

---
+++ 07/material/src/ex13.js
@@ -0,0 +1,109 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+// ----- 주제: MeshStandard에 여러 효과 더하기
+
+export default function example() {
+  // 텍스쳐 이미지 로드
+  const loadingManager = new THREE.LoadingManager();
+  loadingManager.onStart = () => {
+    console.log('로드 시작');
+  };
+  loadingManager.onProgress = (img) => {
+    console.log(img + ' 로드');
+  };
+  loadingManager.onLoad = () => {
+    console.log('로드 완료');
+  };
+  loadingManager.onError = () => {
+    console.log('로드 에러');
+  };
+
+  const textureLoader = new THREE.TextureLoader(loadingManager);
+  const baseColorTex = textureLoader.load(
+    '/textures/brick/Brick_Wall_019_basecolor.jpg'
+  );
+  const ambientTex = textureLoader.load(
+    '/textures/brick/Brick_Wall_019_ambientOcclusion.jpg'
+  );
+  const normalTex = textureLoader.load(
+    '/textures/brick/Brick_Wall_019_normal.jpg'
+  );
+  const roughnessTex = textureLoader.load(
+    '/textures/brick/Brick_Wall_019_roughness.jpg'
+  );
+  const heightTex = textureLoader.load(
+    '/textures/brick/Brick_Wall_019_height.png'
+  );
+
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('#fff');
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('#fff', 0.5);
+  const directionalLight = new THREE.DirectionalLight('#fff', 1);
+  directionalLight.position.set(1, 1, 2);
+  scene.add(ambientLight, directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(3, 3, 3);
+  // const material = new THREE.MeshBasicMaterial({
+  const material = new THREE.MeshStandardMaterial({
+    map: baseColorTex,
+    roughness: 0.3,
+    metalness: 0.3,
+    normalMap: normalTex, // 입체감
+    roughnessMap: roughnessTex, // 빛 반사 정도
+    aoMap: ambientTex, // 그림자
+    aoMapIntensity: 5,
+    color: 'red'
+  });
+  const mesh1 = new THREE.Mesh(geometry, material);
+  scene.add(mesh1);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
